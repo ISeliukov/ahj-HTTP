367ff74c65a4728e0bcda38d3cc5aca8154815cb
Garysi <garysi@rambler.ru>
2023-09-11 08:28:29 +0300

    fix lint err1

--- src/js/app.js
+++ src/js/app.js
@@ -28,20 +28,20 @@ const closeBtnModalAdd = document.querySelector('.close');
 //console.log(modalAdd);
 
 closeBtnModalAdd.addEventListener('click', () => {
-//  console.log('close');
+  //  console.log('close');
   modalAdd.style.display = 'none';
 });
 
 addBtn.addEventListener('click', () => {
-//  console.log('open');
+  //  console.log('open');
   modalAdd.style.display = 'flex';
 });
 
 function render(listList) {
-//  console.log('render');
+  //  console.log('render');
   listList.map((item) => {
     const element = new Item(item);
-//    console.log('element', element);
+    //    console.log('element', element);
     element.pushItem(list);
   });
 
@@ -57,15 +57,21 @@ subscribeForm.addEventListener('submit', (e) => {
   const xhr = new XMLHttpRequest();
   const name = body.get('name');
   const description = body.get('description');
-  const sendObject = 'name=' + encodeURIComponent(name) + '&description=' + encodeURIComponent(description) + '&status=' + encodeURIComponent(false);
-//  console.log(sendObject);
+  const sendObject =
+    'name=' +
+    encodeURIComponent(name) +
+    '&description=' +
+    encodeURIComponent(description) +
+    '&status=' +
+    encodeURIComponent(false);
+  //  console.log(sendObject);
   xhr.onreadystatechange = () => {
     if (xhr.readyState !== 4) return null;
   };
-  
+
   xhr.open('POST', 'http://localhost:7070?method=createTicket');
   xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-//  console.log('==================',sendObject);
+  //  console.log('==================',sendObject);
   xhr.send(sendObject);
   body.delete('name');
   body.delete('description');
@@ -75,7 +81,7 @@ subscribeForm.addEventListener('submit', (e) => {
 
 function addItemFunctionality() {
   const items = Array.from(document.querySelectorAll('.item'));
-//  console.log('ElementsFromLoop', items[0]);
+  //  console.log('ElementsFromLoop', items[0]);
 
   for (let i = 0; i < items.length; i++) {
     const element = items[i];
@@ -83,7 +89,7 @@ function addItemFunctionality() {
     const btnEdit = element.querySelector('.edit');
     const btnCheckbox = element.querySelector('.checkbox');
     const btnDelete = element.querySelector('.delete');
-//    console.log('btnBtn', btnDelete);
+    //    console.log('btnBtn', btnDelete);
 
     btnCheckbox.addEventListener('click', () => {
       btnCheckbox.classList.toggle('true');
@@ -99,14 +105,14 @@ function addItemFunctionality() {
     });
 
     element.addEventListener('click', () => {
-//      console.log('descriotion click');
+      //      console.log('descriotion click');
       const descriotion = element.querySelector('.descriotion');
       if (!descriotion.classList.contains('toggle')) {
-//        console.log('!TOGGLE');
+        //        console.log('!TOGGLE');
         descriotion.style.display = 'flex';
         addDascription(descriotion, id);
       } else {
-//        console.log('TOGGLE');
+        //        console.log('TOGGLE');
         descriotion.style.display = 'none';
         descriotion.textContent = '';
         descriotion.classList.toggle('toggle');
@@ -121,14 +127,14 @@ function addDascription(element, id) {
   xhr.send();
   xhr.addEventListener('load', () => {
     if (xhr.status >= 200 && xhr.status < 300) {
-//      console.log('ответ', xhr.responseText);
+      //      console.log('ответ', xhr.responseText);
       try {
         const data = JSON.parse(xhr.responseText);
-//        console.log('descriotion', data.ticket.description);
+        //        console.log('descriotion', data.ticket.description);
         element.textContent = data.ticket.description;
         element.classList.toggle('toggle');
       } catch (err) {
-//        console.error(err);
+        //        console.error(err);
       }
     }
   });
@@ -138,10 +144,10 @@ function deleteTask(id) {
   modalDelete.style.display = 'flex';
   const yesBtn = modalDelete.querySelector('yes');
   const noBtn = modalDelete.querySelector('no');
-//  console.log(yesBtn, noBtn);
+  //  console.log(yesBtn, noBtn);
   modalDelete.addEventListener('click', (e) => {
     if (e.target.className.includes('no')) {
-//      console.log('e.target', e.target);
+      //      console.log('e.target', e.target);
       modalDelete.style.display = 'none';
     } else {
       const xhr = new XMLHttpRequest();
@@ -150,7 +156,10 @@ function deleteTask(id) {
         return null;
         // alert(xhr.responseText);
       };
-      xhr.open('DELETE', `http://localhost:7070?method=deleteTicketById&id=${id}`);
+      xhr.open(
+        'DELETE',
+        `http://localhost:7070?method=deleteTicketById&id=${id}`
+      );
       xhr.send();
       modalDelete.style.display = 'none';
       getTasks();
@@ -169,13 +178,13 @@ function getTasks() {
   xhr.send();
   xhr.addEventListener('load', () => {
     if (xhr.status >= 200 && xhr.status < 300) {
-//      console.log('ответ', xhr.responseText);
+      //      console.log('ответ', xhr.responseText);
       try {
         const data = JSON.parse(xhr.responseText);
-//        console.log('Data', data);
+        //        console.log('Data', data);
         render(data);
       } catch (err) {
-//        console.error(err);
+        //        console.error(err);
       }
     }
   });
@@ -189,18 +198,18 @@ function editTask(id) {
   const editBtn = modalEdit.querySelector('.save');
   const closeBtn = modalEdit.querySelector('.cansel');
 
-//  console.log(editBtn, closeBtn);
-//  console.log('inputs', inputName, inputDescription);
+  //  console.log(editBtn, closeBtn);
+  //  console.log('inputs', inputName, inputDescription);
 
   const xhr = new XMLHttpRequest();
   xhr.open('GET', `http://localhost:7070?method=ticketById&id=${id}`);
   xhr.send();
   xhr.addEventListener('load', () => {
     if (xhr.status >= 200 && xhr.status < 300) {
-//      console.log('ответ', xhr.responseText);
+      //      console.log('ответ', xhr.responseText);
       try {
         const data = JSON.parse(xhr.responseText);
-//        console.log('descriotion', data.ticket.description);
+        //        console.log('descriotion', data.ticket.description);
         inputName.value = data.ticket.name;
         inputDescription.value = data.ticket.description;
       } catch (err) {
@@ -210,18 +219,24 @@ function editTask(id) {
   });
 
   closeBtn.addEventListener('click', () => {
-//    console.log('close');
+    //    console.log('close');
     modalEdit.style.display = 'none';
   });
-  
+
   updateForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const body = new FormData(updateForm);
 
     const name = body.get('name');
     const description = body.get('description');
-    const sendObject = 'name=' + encodeURIComponent(name) + '&description=' + encodeURIComponent(description) + '&status=' + encodeURIComponent(false);
-//    console.log(sendObject);
+    const sendObject =
+      'name=' +
+      encodeURIComponent(name) +
+      '&description=' +
+      encodeURIComponent(description) +
+      '&status=' +
+      encodeURIComponent(false);
+    //    console.log(sendObject);
     xhr.onreadystatechange = () => {
       if (xhr.readyState !== 4) return null;
     };
